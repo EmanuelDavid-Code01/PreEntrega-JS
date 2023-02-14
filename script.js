6c843d16a57ebc522f290970fa49fedaffb98a00
Emanuel <emadg07.ed@gmail.com>
2023-02-14 03:00:00 -0300

    desafio final completo

--- script.js
+++ script.js
@@ -1,66 +1,64 @@
-let productos = [
-  {
-    id: 1,
-    nombre: "Short Deportivo",
-    categoria: "Deportiva",
-    precio: 1500,
-    stock: 20,
-    imgUrl: "./Imagenes/ShortDeportivo.png",
-  },
-  {
-    id: 2,
-    nombre: "Campera",
-    categoria: "Ropa de invierno",
-    precio: 1800,
-    stock: 10,
-    imgUrl: "./Imagenes/Campera.png",
-  },
-  {
-    id: 3,
-    nombre: "Musculosa",
-    categoria: "Ropa de verano",
-    precio: 2000,
-    stock: 9,
-    imgUrl: "./Imagenes/Musculosa.jpg",
-  },
-  {
-    id: 4,
-    nombre: "Remera",
-    categoria: "Ropa de Invierno",
-    precio: 2500,
-    stock: 15,
-    imgUrl: "./Imagenes/Remera.png",
-  },
-];
-let carrito = [];
+fetch("./productos.json")
+  .then(respu => respu.json())
+  .then(productos => miPrograma(productos))
+  .catch(error => console.log(error))
 
-let ContenedorProductos = document.getElementById("contenedorProductos"); // Uno solo
-let contenedorCarrito = document.getElementById("contenedorCarrito");
-let buscador = document.getElementById("buscador");
-let buscar = document.getElementById("buscar");
-buscar.onclick = filtrar;
+// if (localStorage.getItem("carrito")) {
+//   let carritoEnJSON = localStorage.getItem("carrito");
+//   carrito = JSON.parse(carritoEnJSON);
+// }
+function miPrograma(productos) {
+  let carrito = localStorage.getItem("carrito")
+    ? JSON.parse(localStorage.getItem("carrito"))
+    : [];
 
-renderizarProductos(productos);
+  let ContenedorProductos = document.getElementById("contenedorProductos"); // Uno solo
+  let contenedorCarrito = document.getElementById("contenedorCarrito");
+  let buscador = document.getElementById("buscador");
+  let buscar = document.getElementById("buscar");
+  buscar.onclick = filtrar;
 
-function filtrar(e) {
-  console.log("E", e.target.id);
-  let productosFiltrados = productos.filter(
-    (producto) =>
-      producto.nombre.toLowerCase().includes(buscador.value.toLowerCase()) ||
-      producto.categoria.toLowerCase().includes(buscador.value.toLowerCase())
-  );
-  console.log(productosFiltrados);
-  renderizarProductos(productosFiltrados);
-}
+  let inputMin = document.getElementById("min");
+  let inputMax = document.getElementById("max");
 
-function renderizarProductos(arrayDeProductos) {
-  ContenedorProductos.innerHTML = "";
-  arrayDeProductos.forEach((producto) => {
-    let cardProducto = document.createElement("div");
-    cardProducto.classList.add("producto");
-    cardProducto.id = `card${producto.id}`;
+  let comprar = document.getElementById("comprar");
+  comprar.addEventListener("click", finalizarCompra);
 
-    cardProducto.innerHTML = `
+  renderizarProductos(productos);
+  renderizarCarrito(carrito);
+
+  function finalizarCompra() {
+    localStorage.removeItem("carrito");
+    carrito = [];
+    renderizarCarrito(carrito);
+    mostrarSweetCompra(
+      "Gracias por su compra",
+      "success",
+      2000,
+      false,
+      "imagenGiff"
+    );
+  }
+
+  function filtrar(e) {
+    console.log("E", e.target.id);
+    let productosFiltrados = productos.filter(
+      (producto) =>
+        producto.nombre.toLowerCase().includes(buscador.value.toLowerCase()) ||
+        producto.categoria.toLowerCase().includes(buscador.value.toLowerCase())
+    );
+    console.log(productosFiltrados);
+    renderizarProductos(productosFiltrados);
+  }
+
+  function renderizarProductos(arrayDeProductos) {
+    ContenedorProductos.innerHTML = "";
+    arrayDeProductos.forEach((producto) => {
+      let cardProducto = document.createElement("div");
+      cardProducto.classList.add("producto");
+      cardProducto.id = `card${producto.id}`;
+
+      cardProducto.innerHTML = `
     <h3>${producto.nombre}</h3>
     <p>$${producto.precio}</p>
     <button id=${producto.id}> Agregar al carrito</button>
@@ -68,50 +66,96 @@ function renderizarProductos(arrayDeProductos) {
     
     `;
 
-    if (producto.stock < 10) {
-      cardProducto.classList.add("pocasUnidades");
-      let PocasUnidades = document.createElement(`h6`);
-      PocasUnidades.innerText = "Pocas unidades";
-      cardProducto.appendChild(PocasUnidades);
-    }
-    ContenedorProductos.append(cardProducto);
+      if (producto.stock < 10) {
+        cardProducto.classList.add("pocasUnidades");
+        let PocasUnidades = document.createElement(`h6`);
+        PocasUnidades.innerText = "Pocas unidades";
+        cardProducto.appendChild(PocasUnidades);
+      }
+      ContenedorProductos.append(cardProducto);
 
-    let boton = document.getElementById(producto.id);
-    boton.onclick = agregarAlCarrito;
-  });
-}
-function agregarAlCarrito(e) {
-  let id = e.target.id;
-  let productoBuscado = productos.find((producto) => producto.id == id);
-  let productosEnCarrito = carrito.find(
-    (productos) => productos.id == productoBuscado.id
-  );
-
-  if (productosEnCarrito) {
-    let posicionProducto = carrito.findIndex(
-      (producto) => producto == productosEnCarrito
-    );
-    carrito[posicionProducto].unidades++;
-    carrito[posicionProducto].subtotal ==
-      carrito[posicionProducto].precio * carrito[posicionProducto].unidades;
-  } else {
-    //propiedades + unidades + subtotal
-    productoBuscado.unidades = 1;
-    productoBuscado.subtotal = productoBuscado.precio;
-    carrito.push(productoBuscado);
+      let boton = document.getElementById(producto.id);
+      boton.onclick = agregarAlCarrito;
+    });
   }
-  console.log(carrito);
-  renderizarCarrito(carrito);
-}
+  function agregarAlCarrito(e) {
+    let id = e.target.id;
+    let productoBuscado = productos.find((producto) => producto.id == id);
+    let productosEnCarrito = carrito.find(
+      (productos) => productos.id == productoBuscado.id
+    );
+    mostrarSweetAlert(
+      "top-end",
+      "Producto agregado",
+      "success",
+      "Se agrego al carrito " + productoBuscado.nombre,
+      false,
+      3000,
+      productoBuscado.imgUrl
+    );
 
-function renderizarCarrito(productosEnCarrito) {
-  contenedorCarrito.innerHTML = "";
-  productosEnCarrito.forEach((producto) => {
-    let cardProducto = document.createElement("div");
-    cardProducto.classList.add("itemCarrito");
-    cardProducto.innerHTML += `<h3>${producto.nombre}</h3>
+    if (productosEnCarrito) {
+      let posicionProducto = carrito.findIndex(
+        (producto) => producto == productosEnCarrito
+      );
+      carrito[posicionProducto].unidades++;
+      carrito[posicionProducto].subtotal ==
+        carrito[posicionProducto].precio * carrito[posicionProducto].unidades;
+    } else {
+      //propiedades + unidades + subtotal
+      productoBuscado.unidades = 1;
+      productoBuscado.subtotal = productoBuscado.precio;
+      carrito.push(productoBuscado);
+    }
+    console.log(carrito);
+
+    //storage JSON 1hora 25min
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    renderizarCarrito(carrito);
+  }
+
+  function renderizarCarrito(productosEnCarrito) {
+    contenedorCarrito.innerHTML = "";
+    productosEnCarrito.forEach((producto) => {
+      let cardProducto = document.createElement("div");
+      cardProducto.classList.add("itemCarrito");
+      cardProducto.innerHTML += `<h3>${producto.nombre}</h3>
     <p>${producto.unidades}</P>
     <p>${producto.subtotal}</p>`;
-    contenedorCarrito.appendChild(cardProducto);
-  });
+      contenedorCarrito.appendChild(cardProducto);
+    });
+  }
+
+  function mostrarSweetAlert(
+    posicion,
+    texto,
+    icono,
+    titulo,
+    mostrarBoton,
+    tiempo,
+    urlImagen
+  ) {
+    Swal.fire({
+      position: posicion, //'top-end'
+      text: texto,
+      icon: icono,
+      title: titulo,
+      showConfirmButton: mostrarBoton,
+      timer: tiempo,
+      imageUrl: urlImagen,
+    });
+  }
+  function mostrarSweetCompra(titulo, icono, tiempo, mostrarBoton) {
+    Swal.fire({
+      title: titulo,
+      icon: icono,
+      timer: tiempo,
+      showConfirmButton: mostrarBoton,
+    });
+  }
 }
+fetch(`https://jsonplaceholder.typicode.com/todos`)
+  .then(resp => resp.json())
+  .then(lista => console.log(lista))
+  .catch(error => console.log(error));
